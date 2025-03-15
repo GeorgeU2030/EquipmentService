36710653bb66194d522b5b9ccf36567813583563
CarlosJaBR <carjabo12@gmail.com>
2025-03-15 16:19:08 -0500

    add keycloak and jwt configuration

--- src/main/java/com/equipment/equipmentService/controller/EquipmentController.java
+++ src/main/java/com/equipment/equipmentService/controller/EquipmentController.java
@@ -3,6 +3,7 @@ package com.equipment.equipmentService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +20,13 @@ public class EquipmentController {
     private EquipmentService equipmentService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Equipment addEquipment(@RequestBody Equipment equipment) {
         return equipmentService.addEquipment(equipment);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_TRAINER')")
     public List<Equipment> getAllEquipment() {
         return equipmentService.getAllEquipment();
     }
