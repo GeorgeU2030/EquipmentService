a44d7c908708c7d180ce5243d6bee5d482ffc53b
CarlosJaBR <carjabo12@gmail.com>
2025-04-21 20:31:59 -0500

    feat: edit securityconfig

--- src/main/java/com/equipment/equipmentService/config/SecurityConfig.java
+++ src/main/java/com/equipment/equipmentService/config/SecurityConfig.java
@@ -15,6 +15,21 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import java.util.Collection;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import java.util.Collection;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
